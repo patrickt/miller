dd300e104d68a6bf15c2705da1f6876c8252376d
Patrick Thomson <patrick.william.thomson@gmail.com>
2025-04-23 12:35:08 -0400

    Moremolu.

--- src/Doors.hs
+++ src/Doors.hs
@@ -5,6 +5,8 @@ module Doors
   )
 where
 
+import Control.Effect.Optics
+import Control.Effect.State
 import Control.Monad as X
 import Control.Monad.IO.Class as X
 import Data.Bool as X
@@ -13,22 +15,20 @@ import Data.Foldable as X
 import Data.Function as X
 import Data.Maybe as X
 import Data.Semilattice.Lower as X
-import Prettyprinter as X (Pretty (..), viaShow)
 import Data.Traversable as X
 import Debug.Trace as X
+import Optics (A_Getter, A_Setter, Is, Optic')
+import Prettyprinter as X (Pretty (..), viaShow)
 
-import Optics (Is, A_Setter, A_Getter, Optic')
-import Control.Effect.State
-import Control.Effect.Optics
-
-maybeM :: Applicative f => f a -> Maybe a -> f a
+maybeM :: (Applicative f) => f a -> Maybe a -> f a
 maybeM f = maybe f pure
 
-withinState :: (Is k A_Setter, Is k A_Getter, Has (State s) sig m) => Optic' k is s a -> (a -> (a, b)) -> m b
+withinState ::
+  (Is k A_Setter, Is k A_Getter, Has (State s) sig m) =>
+  Optic' k is s a -> (a -> (a, b)) -> m b
 withinState lens fn = do
   (new, item) <- uses lens fn
   item <$ assign lens new
-
 
 -- One day you'll go
 -- Right now you're here
